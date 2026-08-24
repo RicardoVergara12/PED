5736b42e85d30fd9cc48f1836850eb9679e038e6
Rikiki <arthurrmorales12@gmail.com>
2026-08-23 23:08:09 -0600

    Probando commit

---
+++ holamundo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Hola Mundo!" << endl;
+    cout << "Espero poder salir bien este ciclo :)" << endl;
+    
+    return 0;
+}
