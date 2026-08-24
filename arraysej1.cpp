442f5f8d9a053c5073c818d81da9616372671d01
Rikiki <arthurrmorales12@gmail.com>
2026-08-23 23:59:12 -0600

    añado ej

---
+++ arraysej1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+int edad [5] = {19,20,56,34,25};
+
+for (int i = 0; i < 5; i++)
+{  
+    cout<< edad[i]<<endl;
+}
+
+
+
+
+
+return 0;
+
+}
+
