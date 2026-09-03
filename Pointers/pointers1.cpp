bbea83fec061c27be19f2f14d870a4f9f8c70cfc
Rikiki <arthurrmorales12@gmail.com>
2026-09-02 22:53:39 -0600

    Pointers 1

--- Pointers/pointers1.cpp
+++ Pointers/pointers1.cpp
@@ -1,17 +1,26 @@
 #include<iostream>
 
+/* Un sistema bancario almacena el saldo de una cuenta. Crea una funcion depositar()
+que reciba mediante un puntero el saldo y una cantidad a depositar.
+
+La función debe modificar directamente el saldo original.
+*/
 using namespace std;   
 
+void depositar(float *saldo, float cantidad) {
+    *saldo += cantidad;
+
+}
+
 int main() {
+    float saldo = 1000.0; // Saldo inicial de la cuenta
+    float cantidadDepositar = 500.0; // Cantidad a depositar
 
+    cout << "Saldo inicial: " << saldo << endl;
+    depositar(&saldo, cantidadDepositar);  
+    cout << "Saldo despues del deposito: " << saldo << endl; /*El saldo total despues de
+    hacer el deposito*/
 
-
-
-
-
-
-
-
-
+    return 0;
 
 }
